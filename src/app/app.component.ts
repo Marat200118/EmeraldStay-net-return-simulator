e79b5f06e7e05335a7d7a3cbe1540f2803ef0268
Marat200118 <marat.samigullin1@gmail.com>
2024-09-20 10:02:55 +0200

    added the styling and working on results

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CalculatorComponent } from './calculator/calculator.component';
+import { HeaderComponent } from './header/header.component';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CalculatorComponent, FormsModule],
+  imports: [RouterOutlet, CalculatorComponent, FormsModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
